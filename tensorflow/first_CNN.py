6c4e9bcbb9b145253c60d481ed8d4f433855899e
gaoziqiang <gaoziqianghi@163.com>
2020-10-12 16:31:56 +0800

    init

---
+++ tensorflow/first_CNN.py
@@ -0,0 +1,65 @@
+import tensorflow as tf
+import numpy as np
+
+BATCH_SIZE = 8
+# 设置随机数seed
+
+seed = 23455
+
+# 0:生成数据集
+# 基于seed产生随机数
+rng = np.random.RandomState(seed)
+# 随机数返回32行2列的矩阵，表示32组　有2个特征：体积和重量　作为输入数据集
+X = rng.rand(32,2)
+# 做虚拟标签
+Y = [[int(x0 + x1 < 1)] for (x0,x1) in X]
+
+print('X:\n',X)
+print('Y:\n',Y)
+
+# 1:定义神经网络的输入、参数和输出，定义前向传播过程
+x = tf.placeholder(tf.float32,shape=(None,2))
+y_ = tf.placeholder(tf.float32,shape=(None,1))
+
+w1 = tf.Variable(tf.random_normal([2,3],stddev=1,seed=1))
+w2 = tf.Variable(tf.random_normal([3,1],stddev=1,seed=1))
+
+a = tf.matmul(x,w1)
+y = tf.matmul(a,w2)
+
+# 2:定义损失函数和后向传播方法（优化器）
+loss = tf.reduce_mean(tf.square(y-y_))
+train_step = tf.train.GradientDescentOptimizer(0.001).minimize(loss)
+
+# 3:生成对话，训练STEPS轮
+with tf.Session() as sess:
+    init_op = tf.global_variables_initializer()
+    sess.run(init_op)
+    # 输出目前（未经训练）的参数取值
+    print('---init w1 w2---')
+    print('w1:\n',sess.run(w1))
+    print('w2:\n',sess.run(w2))
+    print('\n')
+
+    # 训练模型
+    STEPS = 3000
+    for i in range(STEPS):
+        start = (i*BATCH_SIZE) % 32
+        end = start + BATCH_SIZE
+
+        # from IPython import embed
+        # embed()
+
+        sess.run(train_step,feed_dict={x:X[start:end],y_:np.mat(Y[start:end])})
+        if i % 500 == 0:
+            total_loss = sess.run(loss,feed_dict={x:X,y_:Y})
+            print('After %d training step(s),loss on all data is %g'%(i,total_loss))
+
+    # 输出训练后的参数取值
+    print('\n')
+    print('---last w1 w2---\n')
+    print('w1:\n',sess.run(w1))
+    print('w2:\n',sess.run(w2))
+
+    from IPython import embed
+    embed()
---
+++ tensorflow/turtle.py
@@ -0,0 +1,6 @@
+import turtle
+
+t = turtle.Pen()
+for i in range(0,4):
+    t.forward(100)
+    t.left(90)
