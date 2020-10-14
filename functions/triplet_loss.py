50a30a65b5edce28893f491c90d6b1cb829d71c7
gaoziqiang <gaoziqianghi@163.com>
2020-10-14 23:02:13 +0800

    add triplet loss function

---
+++ functions/triplet_loss.py
@@ -0,0 +1,62 @@
+from __future__ import absolute_import
+
+import torch
+from torch import nn
+
+class TripletLoss(nn.Module):
+    """Triplet loss with hard positive/negative mining.
+    
+    Reference:
+        Hermans et al. In Defense of the Triplet Loss for Person Re-Identification. arXiv:1703.07737.
+    
+    Imported from `<https://github.com/Cysu/open-reid/blob/master/reid/loss/triplet.py>`_.
+    
+    Args:
+        margin (float, optional): margin for triplet. Default is 0.3.
+    """
+    
+    def __init__(self,margin=0.3):
+        super(TripletLoss, self).__init__()
+        self.margin = margin
+        self.ranking_loss = nn.MarginRankingLoss(margin=margin)
+    def forward(self, inputs, targets):
+        """
+        Args:
+            inputs (torch.Tensor): feature matrix with shape (batch_size, feat_dim).
+            targets (torch.LongTensor): ground truth labels with shape (num_classes).
+        """
+        n = inputs.size(0)
+	# 对数据做归一化处理，很重要
+        inputs = 1. * inputs / (torch.norm(inputs, 2, dim=-1, keepdim=True).expand_as(inputs) + 1e-12)
+        # Compute pairwise distance, replace by the official when merged
+        dist = torch.pow(inputs, 2).sum(dim=1, keepdim=True).expand(n, n)
+        dist = dist + dist.t()
+        dist.addmm_(1, -2, inputs, inputs.t())
+        dist = dist.clamp(min=1e-12).sqrt()  # for numerical stability
+        
+        # For each anchor, find the hardest positive and negative
+        mask = targets.expand(n, n).eq(targets.expand(n, n).t())
+        dist_ap, dist_an = [], []
+        for i in range(n):
+            dist_ap.append(dist[i][mask[i]].max().unsqueeze(0))
+            dist_an.append(dist[i][mask[i] == 0].min().unsqueeze(0))
+        dist_ap = torch.cat(dist_ap)
+        dist_an = torch.cat(dist_an)
+        
+        # Compute ranking hinge loss
+        y = torch.ones_like(dist_an)
+        return self.ranking_loss(dist_an, dist_ap, y)
+
+if __name__ == '__main__':
+    target = [1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8]
+    target = torch.Tensor(target)# 转换为张量Tensor
+    features = torch.Tensor(32,2048)
+    # local_features = torch.randn(32,128,8)
+    # 实例化两个类的实例
+    a = TripletLoss()
+    # b = TripletLossAlignedReID()
+    loss = a(features,target)
+    # print('-----打印a.forward(features,target)----')
+    # print(a.forward(features,target))
+    from IPython import embed
+    embed()
